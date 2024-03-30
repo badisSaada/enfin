463c1454b6ee0d4cd33998315a916c03b67c6cc6
badisSaada <la228671@student.helha.be>
2024-03-30 15:03:09 +0100

    pull request

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,7 +13,7 @@ export class AboutComponent {
   user = {
     name: "Badis",
     email: "brunquersb@helha.be",
-    phone: "+32 498 56 25 12",
+    phone: "+32 498 56 25 13",
     angularLevel: "novice"
   }
 
